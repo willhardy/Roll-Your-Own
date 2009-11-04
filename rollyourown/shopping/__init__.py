e8efd4c7cc845bd81edcd0cced30d65b0e894e43
e.willhardy <e.willhardy@b4d692d0-a4c3-11de-bf1c-ef771b8a44ac>
2009-11-04 07:13:12 +0000

    Adding simple features to session use.

--- rollyourown/shopping/__init__.py
+++ rollyourown/shopping/__init__.py
@@ -25,6 +25,7 @@ from purchase import ModelPurchase, Extra, Items, Total
 from forms import inlineformset_factory
 
 SHOPPING_MODEL_SESSION_KEY = 'ROLLYOUROWN_SHOPPING_%s_PK'
+SHOPPING_MODEL_SESSION_KEY_OLD = 'ROLLYOUROWN_SHOPPING_%s_PK_OLD'
 
 def register(model, model_purchase):
     registry.register(model, model_purchase)
@@ -49,3 +50,22 @@ def get(model, request, create=True):
 
     return get_model(model)._default_manager.get(pk=pk)
 
+
+def get_previous_from_session(model, request):
+    old_order_ids = request.session.get(SHOPPING_MODEL_SESSION_KEY_OLD % model.__name__, set())
+    current_order_id = request.session.get(SHOPPING_MODEL_SESSION_KEY % model.__name__, None)
+    if current_order_id is not None:
+        old_order_ids.add(current_order_id)
+    return get_model(model)._default_manager.filter(id__in=old_order_ids)
+
+
+def clear_current(model, request):
+    try:
+        order_id = request.session.pop(SHOPPING_MODEL_SESSION_KEY % model.__name__)
+
+        # Add order ID to the list of previous orders
+        old_orders = request.session.get(SHOPPING_MODEL_SESSION_KEY_OLD % model.__name__, set())
+        old_orders.add(order_id)
+        request.session[SHOPPING_MODEL_SESSION_KEY_OLD % model.__name__] = old_orders
+    except KeyError:
+        pass
